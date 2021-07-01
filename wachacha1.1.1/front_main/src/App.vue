e65bea184aa43d86ac98cd337d516fe174843b58
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-01 15:43:23 +0900

    7/1

---
+++ wachacha1.1.1/front_main/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="background">
+        <div class="backScreen">
+            <img :src="screen[0].first" />
+        </div>
+        <div class="redBox"></div>
+        <!-- <img :src="screen[0].second" /> -->
+        <!-- <img src="./assets/main/first.jpg" /> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {},
+    data() {
+        return {
+            screen: [
+                {
+                    first: require("@/assets/main/first.jpg"),
+                    second: require("@/assets/main/second.jpg"),
+                    third: require("@/assets/main/third.png"),
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+.background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+.backScreen {
+    position: relative;
+    width: 100px;
+    height: 100px;
+    margin: 0px;
+    padding: 0px;
+}
+.redBox {
+    width: 100px;
+    height: 100px;
+    margin: 0px;
+    padding: 0px;
+    background-color: red;
+}
+</style>
