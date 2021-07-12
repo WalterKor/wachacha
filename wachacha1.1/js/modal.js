957536e485e5db8e26b7bbf94c4ff0181dfaccae
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-12 22:43:19 +0900

    7/12

---
+++ wachacha1.1/js/modal.js
@@ -0,0 +1,9 @@
+loginBtnElem = document.querySelector('.loginBtn');
+
+if(loginBtnElem){    
+    loginBtnElem.addEventListener('click', function(){
+       console.log('ddd');
+       alert('ddd');
+    });
+
+}
